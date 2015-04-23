3ab124f7965f33ed98c6eb9efb71c78a351083e4
Fabiola <fab.tames@gmail.com>
2015-04-22 22:09:49 -0600

    avances

--- busqueda_factura.php
+++ busqueda_factura.php
@@ -3,8 +3,7 @@ require_once("conexion.php");
 
 $mensaje = "";
 $codigo_articulo = $_POST["codigo_articulo"];
-$codigo_empleado= $_POST["codigo_empleado"];
-$num_factura = $_POST["numero_factura"];
+$numero_factura = $_POST["numero_factura"];
 $precio = "";
 $codigo_categoria = "";
 $descripcion = "";
@@ -15,9 +14,9 @@ $disponible = "";
 $total_acumulado = "";
 
 try{
-	$query = mysql_query("SELECT descripcion, precio, descuento, codigo_categoria, disponible FROM inventario where codigo_articulo='$codigo_articulo'",$conexion);
-	$queryEmpleado = mysql_query("SELECT nombre FROM personal where codigo_empleado = '$codigo_empleado'", $conexion);
-	$queryFactura = mysql_query("SELECT SUM(total_detalle) AS 'Total_Acumulado' FROM ventas WHERE numero_factura = '$num_factura'", $conexion);
+	$query = mysql_query("SELECT descripcion, precio, descuento, codigo_categoria, disponible FROM inventario where codigo_articulo='$codigo_articulo and numero_factura=$numero_factura'",$conexion);
+	//$queryEmpleado = mysql_query("SELECT nombre FROM personal where codigo_empleado = '$codigo_empleado'", $conexion);
+	//$queryFactura = mysql_query("SELECT SUM(total_detalle) AS 'Total_Acumulado' FROM ventas WHERE numero_factura = '$num_factura'", $conexion);
 	//echo $cantidad;
 	if (!$query){
 		throw new Exception (mysql_error($query));
@@ -36,22 +35,6 @@ try{
 		}
 	}
 
-	if (!$queryEmpleado){
-		throw new Exception (mysql_error($queryEmpleado));
-	}else{
-		while($row = mysql_fetch_array($queryEmpleado)){
-			$nombre_empleado = $row['nombre'];
-		}
-	}
-
-	if (!$queryFactura){
-		throw new Exception (mysql_error($queryFactura));
-	}else{
-		while($row = mysql_fetch_array($queryFactura)){
-			$total_acumulado = $row['Total_Acumulado'];
-			//echo $total_acumulado;
-		}
-	}
 
    echo json_encode(array(
             'descripcion' => $descripcion,
